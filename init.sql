9ec39b1c20782da96ae34a32c7a3520e704e1cab
Sam Keller <samskeller@gmail.com>
2019-08-27 09:37:12 -0600

    Ensuring the sessions table is created on app startup, should be done by default but seems like it intermittently doesn't

--- init.sql
+++ init.sql
@@ -2,6 +2,20 @@ CREATE DATABASE IF NOT EXISTS `app` DEFAULT CHARACTER SET `utf8`;
 
 USE `app`;
 
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `session_id` varchar(128) COLLATE utf8mb4_bin NOT NULL,
+  `expires` int(11) unsigned NOT NULL,
+  `data` mediumtext COLLATE utf8mb4_bin,
+  PRIMARY KEY (`session_id`)
+) ENGINE=InnoDB;
+
 CREATE DATABASE IF NOT EXISTS `app_test` DEFAULT CHARACTER SET `utf8`;
 
 USE `app_test`;
+
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `session_id` varchar(128) COLLATE utf8mb4_bin NOT NULL,
+  `expires` int(11) unsigned NOT NULL,
+  `data` mediumtext COLLATE utf8mb4_bin,
+  PRIMARY KEY (`session_id`)
+) ENGINE=InnoDB;
